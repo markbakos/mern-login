5a5e230ea4f4dd081595c091709ac56b1a93ae95
mrkdsoftware <mrkdsoftware@gmail.com>
2024-11-07 12:02:32 +0100

    Updated CORS

--- backend/app.js
+++ backend/app.js
@@ -3,25 +3,27 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
+const path = require('path');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-const allowedOrigins = ['https://mern-login-1klb.onrender.com/login', 'https://mern-login-1klb.onrender.com/register']
-app.use(cors({
-    origin: function (origin, callback) {
-        if(allowedOrigins.includes(origin) || !origin) {
-            callback(null, true)
-        } else{
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-}));
+
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+
+app.use(cors({
+    origin: 'https://mern-login-1klb.onrender.com'
+}))
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.get('/', (req,res) => {
     res.send('Backend is running!');
