f5236a717579b3afec041e500b2b60bedb046829
mrkdsoftware <mrkdsoftware@gmail.com>
2024-11-07 11:38:14 +0100

    Adding deploy

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
         e.preventDefault();
 
         try{
-            const response = await axios.post( process.env.BACKEND_URL + '/api/auth/login', {
+            const response = await axios.post( 'https://mern-login-backend-vwgd.onrender.com/api/auth/login', {
                 username,
                 password
             })
