ebb9887482c42c63fdb9c7e66da57803c21d28fe
mrkdsoftware <mrkdsoftware@gmail.com>
2024-11-07 11:40:53 +0100

    Adding deploy

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://mern-login-1klb.onrender.com',
+}));
+
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
